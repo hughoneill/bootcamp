50be105a372e4ffdcde2edf6fae597a98d41dec6
Hugh O'Neill <hughone@gmail.com>
2018-07-13 11:37:43 -0400

    new lessons 18 & 19, PEP 8 on *.py files

--- my_files/seq_features.py
+++ my_files/seq_features.py
@@ -5,6 +5,7 @@ def is_valid_sequence(seq):
         if aa not in bootcamp_utils.aa.keys():
             raise RuntimeError(aa + ' is not a valid amino acid.')
 
+            
 def number_negatives(seq):
     """Number of negative residues a protein sequence"""
     # Convert sequence to upper case
@@ -15,6 +16,7 @@ def number_negatives(seq):
 
     # Count E's and D's, since these are the negative residues
     return seq.count('E') + seq.count('D')
+
 
 def number_positives(seq):
     """Number of positive residues a protein sequence"""
